898e1febe0aab8a418954be2afd25cd813defe8a
Your Name <you@example.com>
2020-04-18 15:31:56 -0300

    fixes

--- bin-ftx-mm.py
+++ bin-ftx-mm.py
@@ -616,16 +616,20 @@ class MarketMaker( object ):
         if gogo == True:
             if self.positions[fut + '-binance']['size'] > self.MAX_SKEW * 2 and self.place_bids[token] == False and self.place_asks[token] == False:
                 self.binance.createOrder(  fut + '/USDT', "Limit", 'sell', qty, self.get_bbo('binance', fut)['bid'], {"newClientOrderId": "x-v0tiKJjj-" + self.randomword(15)})
-                print('sell bin')
+                print('sell bin, return')
+                return
             if self.positions[fut + '-binance']['size'] < -1 * self.MAX_SKEW * 2 and self.place_bids[token] == False and self.place_asks[token] == False:
                 self.binance.createOrder(  fut + '/USDT', "Limit", 'buy', qty, self.get_bbo('binance', fut)['ask'], {"newClientOrderId": "x-v0tiKJjj-" + self.randomword(15)})
-                print('buy bin')
+                print('buy bin, return')
+                return
             if self.positions[fut + '-ftx']['size'] > self.MAX_SKEW * 2 and self.place_bids[token] == False and self.place_asks[token] == False:
                 self.ftx.createOrder(  fut + '-PERP', "limit", 'sell', qty, self.get_bbo('ftx', fut)['bid'], {"newClientOrderId": "x-v0tiKJjj-" + self.randomword(15)})
-                print('sell ftx')
+                print('sell ftx, return')
+                return
             if self.positions[fut + '-ftx']['size'] < -1 * self.MAX_SKEW * 2 and self.place_bids[token] == False and self.place_asks[token] == False:
                 self.ftx.createOrder(  fut + '-PERP', "limit", 'buy', qty, self.get_bbo('ftx', fut)['ask'], {"newClientOrderId": "x-v0tiKJjj-" + self.randomword(15)})
-                print('buy ftx')
+                print('buy ftx, return')
+                return
             if self.place_asks[token] == False and self.place_bids[token] == False and (math.fabs(self.positions[fut+'-binance']['size']) > 15 or math.fabs(self.positions[fut+'-ftx']['size']) > 15):
                 
 
@@ -916,7 +920,8 @@ class MarketMaker( object ):
             t_now   = datetime.datetime.utcnow()
             
             # Update time series and vols
-            for token in self.futures:
+            for token in self.positions:
+                token = token.split('-')[0]
                 self.place_orders('binance', token)
                 
                 self.place_orders('ftx', token)
